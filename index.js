706aed50a22f6d9a112bcb3a204da6765ed8a884
Logan Zander <Loganrayzander@gmail.com>
2023-01-22 11:21:31 -0600

    index.html

--- index.js
+++ index.js
@@ -33,3 +33,10 @@ const sandwhiches=[
       ]
     }
   ]
+
+  const element = document.getElementById("myBtn");
+  element.addEventListener("click", myFunction);
+  
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
